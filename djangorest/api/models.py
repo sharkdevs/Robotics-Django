e274bc9e88b9b5775d184eacda429fb5ecbb29bd
joma simeon <jomasim@jomas-MacBook-Pro.local>
2018-12-20 14:47:22 +0300

    [#162779226] add bucketlist model and serializer

--- djangorest/api/models.py
+++ djangorest/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Bucketlist(models.Model):
+    ''' buckectlist model '''
+    title=models.CharField(max_length=50)
+    date_created=models.DateField(auto_now_add=True)
+    date_modified=models.DateField(auto_now=True)
+
+    def ___str__(self):
+        return "{}".format(self.title)
+   
+
+
