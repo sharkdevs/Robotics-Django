0b8cfcb83c3ce3f3ca43686bd4e30de5a3bceae7
warachi <morriswarachi0@gmail.com>
2018-12-20 19:38:54 +0300

    [Feature #162778391] add Register view and create empty serializer file

--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views  import status
 
-# Create your views here.
+class RegisterUsers(generics.CreateAPIView):
+    """POST auth/register/"""
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        email = request.data.get("email", "")
+        password = request.data.get("password", "")
+        if not email and not password:
+            return Response(
+                data={
+                    "message": "email, password and email is required to register a user"
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        new_user = User.objects.create_user(
+            email=email, password=password, username=''
+        )
+        return Response(
+            data=UserSerializer(new_user).data,
+            status=status.HTTP_201_CREATED
+        )
