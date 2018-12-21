981decb29279be984faa26437e8f658ceab38c83
KE-C17RF005FVH3 <ke-c17rf005fvh3@KE-C17RF005FVH3s-MacBook-Pro.local>
2018-12-21 06:26:48 +0300

    [#162803796] Add bucketlist delete method

--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -12,3 +12,9 @@ class CreateView(generics.ListCreateAPIView):
     def perform_create(self,serializer):
         '''save new bucketlist data '''
         serializer.save()
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """ This class handles the http GET, PUT and DELETE requests"""
+
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
