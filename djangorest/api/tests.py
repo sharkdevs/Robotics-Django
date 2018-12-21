5bae052a8a8217c2dfed5745eeb01f5abea80519
KE-C17RF005FVH3 <ke-c17rf005fvh3@KE-C17RF005FVH3s-MacBook-Pro.local>
2018-12-21 06:57:36 +0300

    [Finishes #162803796] Add bucketlist url path

--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
 from .models import Bucketlist
