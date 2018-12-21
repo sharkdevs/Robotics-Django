e92c773045d9393d0be2090182cbf240ddf42670
johnwayodi <wayodijohn@gmail.com>
2018-12-21 06:22:30 +0300

    [Feature #162768718] Add details view for single bucketlist item

--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -1,14 +1,22 @@
 from rest_framework import generics
 from .serializers import BucketlistSerializer
-from  .models import Bucketlist
+from .models import Bucketlist
+
 
 # Create your views here.
 
 class CreateView(generics.ListCreateAPIView):
-    ''' defining create behavior for the rest api '''
-    queryset=Bucketlist.objects.all()
-    serializer_class=BucketlistSerializer
+    """ defining create behavior for the rest api """
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
 
-    def perform_create(self,serializer):
-        '''save new bucketlist data '''
+    def perform_create(self, serializer):
+        """ save new bucketlist data """
         serializer.save()
+
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
