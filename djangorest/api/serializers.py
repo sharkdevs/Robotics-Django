af01378f71c02fdb7abc4588ddecaef09a0e79de
johnwayodi <wayodijohn@gmail.com>
2018-12-21 06:26:03 +0300

    [Feature #162768718] Change docstring to triple quotes

--- djangorest/api/serializers.py
+++ djangorest/api/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from .models import Bucketlist
 
+
 class BucketlistSerializer(serializers.ModelSerializer):
-    ''' map the model into json '''
+    """ map the model into json """
 
     class Meta:
-        ''' meta class maps serializer to model fields'''
-        model=Bucketlist
-        fields=('id','title','date_created','date_modified')
-        read_only_fields=('date_created','date_modified')
+        """ meta class maps serializer to model fields"""
+        model = Bucketlist
+        fields = ('id', 'title', 'date_created', 'date_modified')
+        read_only_fields = ('date_created', 'date_modified')
